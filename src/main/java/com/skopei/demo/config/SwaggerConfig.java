412ebbeb4508b536a7a013ee04f4b262c37348bc
foobar <18009395@student.hhs.nl>
2022-10-04 18:12:12 +0200

    swagger config file

---
+++ src/main/java/com/skopei/demo/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.skopei.demo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPIConfig() {
+        Info info = new Info();
+        info
+                .title("Skopei API")
+                .description("exercise api")
+                .version("v1.0.0");
+        return new OpenAPI().info(info);
+    }
+}
